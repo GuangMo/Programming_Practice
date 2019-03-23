cb192c97b04df02f83c342f1acdc817c53d9ffa4
Guang Mo <guang.s.mo@gmail.com>
2019-03-23 08:04:06 -0700

    added example on how to find a value in map c++

--- CplusplusQuickReference.cpp
+++ CplusplusQuickReference.cpp
@@ -3,10 +3,19 @@ This file provides example on variable data structure usages in c++
 */
 
 #include <map> 
-
+#include <iostream> 
 
 int main() 
 {
-  /* Find a value of from a map example */ 
+  /* Find a value from a map example */
+  std::map<char,int> mymap;
+  std::map<char,int>::iterator it;
+  mymap['b']=100;
+  it = mymap.find('b');
+  if (it != mymap.end())
+    mymap.erase (it);
+  /* End of find a value from a map example */ 
+
+  
   return 0; 
 }
